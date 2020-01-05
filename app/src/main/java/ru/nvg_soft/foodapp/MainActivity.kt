9bd9f59b0b9ca30354ab39da3c518a64743fca57
DaniilShcherbakov <Charel@bk.ru>
2020-01-05 13:26:19 +0300

    Load gridView with list of food

--- app/src/main/java/ru/nvg_soft/foodapp/MainActivity.kt
+++ app/src/main/java/ru/nvg_soft/foodapp/MainActivity.kt
@@ -1,12 +1,64 @@
 package ru.nvg_soft.foodapp
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.food_tiket.view.*
 
 class MainActivity : AppCompatActivity() {
 
+    var listOfFood = ArrayList<Food>()
+    var adapter:FoodAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //load list
+        listOfFood.add(Food("Coffee","Описание содержащее множество букв для того чтобы клиент все понял",R.drawable.coffee_pot))
+        listOfFood.add(Food("Espresso","Описание содержащее множество букв для того чтобы клиент все понял",R.drawable.espresso))
+        listOfFood.add(Food("French fries","Описание содержащее множество букв для того чтобы клиент все понял",R.drawable.french_fries))
+        listOfFood.add(Food("Honey","Описание содержащее множество букв для того чтобы клиент все понял",R.drawable.honey))
+        listOfFood.add(Food("Strawberry ice cream","Описание содержащее множество букв для того чтобы клиент все понял",R.drawable.strawberry_ice_cream))
+        listOfFood.add(Food("sugar cubes","Описание содержащее множество букв для того чтобы клиент все понял",R.drawable.sugar_cubes))
+
+        adapter = FoodAdapter(this, listOfFood)
+        gvListFood.adapter = adapter
+    }
+
+    class FoodAdapter: BaseAdapter {
+        var context:Context? = null
+        var listOfFood = ArrayList<Food>()
+
+        constructor(context: Context, listOfFood:ArrayList<Food>){
+            this.listOfFood = listOfFood
+            this.context = context
+
+        }
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            val food = listOfFood[p0]
+            var inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            var foodView = inflater.inflate(R.layout.food_tiket,null)
+            foodView.ivFood.setImageResource(food.image!!)
+            foodView.tvName.text = food.name
+            return foodView
+        }
+
+        override fun getItem(p0: Int): Any {
+           return listOfFood[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+           return p0.toLong()
+        }
+
+        override fun getCount(): Int {
+          return listOfFood.size
+        }
+
     }
 }
